b23952fa21d91dcf2b151bee94b97f9c928b06ed
Sujit <sujitpranav@kreatio.com>
2015-02-28 11:40:31 +0530

    changes

---
+++ myapp/db/migrate/20150228053727_create_blogs.rb
@@ -0,0 +1,11 @@
+class CreateBlogs < ActiveRecord::Migration
+  def change
+    create_table :blogs do |t|
+      t.string :name
+      t.string :phone
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
